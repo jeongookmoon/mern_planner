47121f074eda692fe6b7fb948bc746e722740cfd
Jeong Ook Moon <moonjeongook@gmail.com>
2020-08-10 23:03:33 -0700

    add build dir for production

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -24,6 +24,15 @@ const usersRouter = require('./routes/users');
 app.use('/plans', plansRouter);
 app.use('/users', usersRouter);
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
