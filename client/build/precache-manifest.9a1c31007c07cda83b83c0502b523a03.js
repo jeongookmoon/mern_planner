20bfbed4fd3d9a64a9034b477b970985ac1a72ea
Jeong Ook Moon <moonjeongook@gmail.com>
2020-08-11 21:15:27 -0700

    include build for frontend

---
+++ client/build/precache-manifest.9a1c31007c07cda83b83c0502b523a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1aaa7813327ba19a7b8ee9c32694d09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61b064ff9434e15a6224",
+    "url": "/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "61b064ff9434e15a6224",
+    "url": "/static/js/2.a74ac1d6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.a74ac1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b955fbea25b60d2acbd9",
+    "url": "/static/js/main.d382078f.chunk.js"
+  },
+  {
+    "revision": "c894d4e7a8b5725c7657",
+    "url": "/static/js/runtime-main.4314310f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -26,11 +26,11 @@ app.use('/users', usersRouter);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
