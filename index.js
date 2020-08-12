16e9f5d959937d285b4ff91e525d3d5ae414190e
Jeong Ook Moon <moonjeongook@gmail.com>
2020-08-11 21:23:38 -0700

    set port

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.set("port", port);
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
@@ -34,6 +35,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
+app.listen(app.get('port'), () => {
+  console.log(`Server is running at ${app.get('port')}`);
 });
